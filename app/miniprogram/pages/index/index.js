05fb499abdc96110b0a824c190b5616ae418bd7b
Volcano-Yang <648941183@qq.com>
2021-05-27 17:16:12 +0800

    ✨  feat(ui): 初始化上传组件ui

---
+++ app/miniprogram/pages/index/index.js
@@ -0,0 +1,59 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgList: [],
+    imgMaxNumber: 4
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  ChooseImage() {
+    wx.chooseImage({
+      count: this.data.imgMaxNumber, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        if (this.data.imgList.length != 0) {
+          this.setData({
+            imgList: this.data.imgList.concat(res.tempFilePaths)
+          })
+        } else {
+          this.setData({
+            imgList: res.tempFilePaths
+          })
+        }
+      }
+    });
+  },
+
+  ViewImage(e) {
+    wx.previewImage({
+      urls: this.data.imgList,
+      current: e.currentTarget.dataset.url
+    });
+  },
+
+  DelImg(e) {
+    wx.showModal({
+      title: '确定删除这张图片吗？',
+      cancelText: '再看看',
+      confirmText: '确定',
+      success: res => {
+        if (res.confirm) {
+          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+          this.setData({
+            imgList: this.data.imgList
+          })
+        }
+      }
+    })
+  },
+})
